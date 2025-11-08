c6e862768bc87bd4d01d7c7c60c582dcced9284b
Evan Solecki <etsm2n@umsystem.com>
2025-11-08 14:35:17 -0600

    added frontend buttons (again)

--- frontend/top_buttons.js
+++ frontend/top_buttons.js
@@ -17,7 +17,7 @@ function Clicklayout() {
 
 
 const resetButton = makeButton(stage.width()-200, 0, 100, navbarHeight,'Reset', uiLayer, clickReset);
-const layoutButton = makeButton(stage.width()-300, 0, 100, navbarHeight,'Delete', uiLayer, clickDelete);
+const layoutButton = makeButton(stage.width()-300, 0, 100, navbarHeight,'Clear', uiLayer, clickDelete);
 const eraseButton = makeButton(stage.width()-400, 0, 100, navbarHeight,'Layout', uiLayer, Clicklayout);
 
 
